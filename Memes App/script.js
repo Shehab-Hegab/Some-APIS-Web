ec138d5005bec139d66e32328201844fd1c1f6ba
Shehab-Hegab <137138481+Shehab-Hegab@users.noreply.github.com>
2023-09-23 16:18:54 +0300

    Update script.js

--- Memes App/script.js
+++ Memes App/script.js
@@ -1,30 +1,38 @@
 let meme = document.getElementById("meme");
 let title = document.getElementById("title");
 let getMemeBtn = document.getElementById("get-meme-btn");
-//API URL
-let url = " https://meme-api.herokuapp.com/gimme/";
-//Array of subreddits of your choice
+// API URL
+let url = "https://meme-api.com/gimme/";
+// Array of subreddits of your choice
 let subreddits = ["catmemes", "wholesomemes", "dogmemes", "me_irl"];
 
-//Function To Get Random Meme
+// Function To Get Random Meme
 let getMeme = () => {
-  //Choose a random subreddit from the subreddits array
+  // Choose a random subreddit from the subreddits array
   let randomSubreddit =
     subreddits[Math.floor(Math.random() * subreddits.length)];
-  //Fetch data from the api
+  // Fetch data from the api
   fetch(url + randomSubreddit)
-    .then((resp) => resp.json())
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return resp.json();
+    })
     .then((data) => {
       let memeImg = new Image();
-      //Display meme image and title only after the image loads
+      // Display meme image and title only after the image loads
       memeImg.onload = () => {
         meme.src = data.url;
         title.innerHTML = data.title;
       };
       memeImg.src = data.url;
+    })
+    .catch((error) => {
+      console.error("Error fetching meme:", error);
     });
 };
 
-//Call the getMeme() on button click and on window load
+// Call the getMeme() on button click and on window load
 getMemeBtn.addEventListener("click", getMeme);
 window.addEventListener("load", getMeme);
